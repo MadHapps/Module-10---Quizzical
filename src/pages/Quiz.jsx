4b569c0dddae079b845622a8a07861330d1e2e8a
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-05 16:12:22 -0700

    Quiz page component updated

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,10 +1,32 @@
 import "./Quiz.css";
 import { Link } from "react-router-dom";
+import Question from "../components/Question";
+import { useEffect, useState } from "react";
 
 export default function Quiz() {
+  const sessionId = sessionStorage.key("sessionId")
+    ? sessionStorage.getItem("sessionId")
+    : fetch("https://opentdb.com/api_token.php?command=request")
+        .then((res) => res.json())
+        .then((data) => {
+          sessionStorage.setItem("sessionId", data.token);
+          return data.token;
+        });
+
+  const [questions, setQuestions] = useState(null)
+      
+  !questions &&
+  fetch(`https://opentdb.com/api.php?amount=5&encode=url3986&token=${sessionId}`)
+    .then((res) => res.json())
+    .then((data) => setQuestions(data.results));
+
+
+
   return (
     <main className="page-wrapper">
-      <h1>This is the quiz page~!</h1>
+      {questions && questions.map((question, index) => (
+        <Question key={index} data={question} />
+      ))}
       <Link to="/review" className="router-link-button">
         Check answers
       </Link>
