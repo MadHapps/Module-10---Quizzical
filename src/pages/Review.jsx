848a62d6f8304d1f8373f02621b873871e8d567e
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-08 20:17:54 -0700

    Created Review page component

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,9 +1,29 @@
+/* eslint-disable react/prop-types */
 import "./Review.css";
+import { Link } from "react-router-dom";
+import ReviewedQuestion from "../components/ReviewedQuestion";
 
-export default function Review() {
+export default function Review({ questions }) {
+  const correctAnswerCount = questions.filter(
+    (question) => question.isCorrect === true
+  ).length;
+
   return (
-    <main>
-      <h1>This is the review page~!</h1>
+    <main className="page-wrapper">
+      {questions &&
+        questions.map((question) => (
+          <ReviewedQuestion
+            key={question.id}
+            data={question}
+            allAnswers={question.allAnswers}
+          />
+        ))}
+      <section className="results-wrapper">
+        <h2>
+          You scored {correctAnswerCount}/{questions.length} correct answers!
+        </h2>
+        <Link to="/">Play again</Link>
+      </section>
     </main>
   );
 }
