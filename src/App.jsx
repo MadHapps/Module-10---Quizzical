ea981b1cc3bb036cb8da9b0d025d0d3416741e64
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-08 20:23:17 -0700

    Updated App component, now contains questions state

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,75 @@ import "./App.css";
 import Front from "./pages/Front";
 import Quiz from "./pages/Quiz";
 import Review from "./pages/Review";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [questions, setQuestions] = useState(
+    JSON.parse(sessionStorage.getItem("questions")) || null
+  );
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  function generateNewQuestions() {
+    fetch(`https://opentdb.com/api.php?amount=5&difficulty=easy&encode=url3986`)
+      .then((res) => res.json())
+      .then((data) =>
+        setQuestions(() => {
+          return data.results.map((question) => {
+            const incorrectAnswers = question.incorrect_answers.map((answer) =>
+              decodeURIComponent(answer)
+            );
+            const correctAnswer = decodeURIComponent(question.correct_answer);
+            let allAnswers = [correctAnswer, ...incorrectAnswers];
+            allAnswers.length === 2
+              ? (allAnswers = ["True", "False"])
+              : (allAnswers = shuffleArray(allAnswers));
+
+            return {
+              ...question,
+              category: decodeURIComponent(question.category),
+              correct_answer: correctAnswer,
+              incorrect_answers: incorrectAnswers,
+              allAnswers: allAnswers,
+              question: decodeURIComponent(question.question),
+              id: nanoid(),
+              selected: false,
+            };
+          });
+        })
+      );
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Front />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/review" element={<Review />} />
+        <Route
+          path="/"
+          element={
+            <Front
+              setQuestions={setQuestions}
+              generateNewQuestions={generateNewQuestions}
+            />
+          }
+        />
+        <Route
+          path="/quiz"
+          element={
+            <Quiz
+              questions={questions}
+              setQuestions={setQuestions}
+              generateNewQuestions={generateNewQuestions}
+            />
+          }
+        />
+        <Route path="/review" element={<Review questions={questions} />} />
       </Routes>
     </BrowserRouter>
   );
