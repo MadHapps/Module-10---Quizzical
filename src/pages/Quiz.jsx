656eb0f93b655e352e80fa87841770610ee0ccdf
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-08 20:19:29 -0700

    Updated Quiz page component

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,33 +1,55 @@
+/* eslint-disable react/prop-types */
 import "./Quiz.css";
 import { Link } from "react-router-dom";
 import Question from "../components/Question";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-export default function Quiz() {
-  const sessionId = sessionStorage.key("sessionId")
-    ? sessionStorage.getItem("sessionId")
-    : fetch("https://opentdb.com/api_token.php?command=request")
-        .then((res) => res.json())
-        .then((data) => {
-          sessionStorage.setItem("sessionId", data.token);
-          return data.token;
-        });
+export default function Quiz({
+  questions,
+  setQuestions,
+  generateNewQuestions,
+}) {
+  useEffect(() => {
+    sessionStorage.setItem("questions", JSON.stringify(questions));
+  }, [questions]);
 
-  const [questions, setQuestions] = useState(null)
-      
-  !questions &&
-  fetch(`https://opentdb.com/api.php?amount=5&difficulty=easy&encode=url3986&token=${sessionId}`)
-    .then((res) => res.json())
-    .then((data) => setQuestions(data.results));
+  !questions && generateNewQuestions();
 
-
+  function handleSelection(id, selection, answer) {
+    setQuestions((prevQuestions) => {
+      return prevQuestions.map((question) => {
+        if (question.id === id) {
+          const isCorrect = selection === answer;
+          return {
+            ...question,
+            selected: selection,
+            isCorrect: isCorrect,
+          };
+        }
+        return question;
+      });
+    });
+  }
 
   return (
     <main className="page-wrapper">
-      {questions && questions.map((question, index) => (
-        <Question key={index} data={question} />
-      ))}
-      <Link to="/review" className="router-link-button">
+      <button onClick={generateNewQuestions}>Generate New Questions</button>
+      {questions &&
+        questions.map((question) => (
+          <Question
+            key={question.id}
+            id={question.id}
+            data={question}
+            allAnswers={question.allAnswers}
+            onClick={handleSelection}
+            selected={question.selected}
+            isCorrect={question.isCorrect || null}
+          />
+        ))}
+      <Link
+        to={{ pathname: "/review", state: { questions } }}
+        className="router-link-button"
+      >
         Check answers
       </Link>
     </main>
